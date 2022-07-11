d600ad433d5618b79fa67f99975ff58f9f7ee3db
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-07-12 01:35:12 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@
 public class Main {
 	public static void main(String[] args) {
 		//HelloWorldを出力
+		System.out.println("①　HelloWorldを出力");
 		System.out.println("HelloWorld");
+		System.out.println("");
 		
         //HelloWorldを10回出力
+		System.out.println("②　HelloWorldを10回出力");
 		for (int i = 0; i < 10; i++) {
 			System.out.println("HelloWorld");
 		}
+		System.out.println("");
 		
 		//11回目はHelloだけ出力
+		System.out.println("③　11回目はHelloだけ出力");
 		for (int i = 0; i < 11; i++) {
 			if(i < 10) {
 				System.out.println("HelloWorld");
