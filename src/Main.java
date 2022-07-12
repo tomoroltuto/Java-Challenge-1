32d6c4a6f77e55d07096f6fc399671a0378fa7d5
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-07-12 22:28:08 +0900

    fix:演算子"<"を"<="に修正

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
 		
 		//HelloWorldを10回出力
 		System.out.println("②　HelloWorldを10回出力");
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i <= 10; i++) {
 			System.out.println("HelloWorld");
 		}
 		System.out.println("");
 		
 		//11回目はHelloだけ出力
 		System.out.println("③　11回目はHelloだけ出力");
-		for (int i = 0; i < 11; i++) {
+		for (int i = 0; i <= 10; i++) {
 			if(i < 10) {
 				System.out.println("HelloWorld");
 			} else {
